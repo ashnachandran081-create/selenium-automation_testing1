41ee6beab225f2a91448a6c11cc117f60b79aed6
jishnu raj <you@example.com>
2026-04-01 02:15:39 +0530

    setup all for passing -excel sheet as well

--- src/test/java/tests/Testtest.java
+++ src/test/java/tests/Testtest.java
@@ -2,7 +2,9 @@ package tests;
 
 public class Testtest {
 public void status() {
+	int a=5;int b=8;
 	System.out.println("no way");
 	System.out.println("helllooooo");
+	System.out.println(a+b);
 }
 }
